6c9e611d595333e038d952de7f1246a02486e6cd
Franck De Goër <degoerdf@ensimag.fr>
2013-09-07 19:14:07 +0200

    Deployment fixes

--- website/seventeen/urls.py
+++ website/seventeen/urls.py
@@ -16,6 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^prime/', include('primes.urls')),
+    url(r'^', include('primes.urls')),
 
 )
